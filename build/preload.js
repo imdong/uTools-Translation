c969a0bd782c548e969a5e730355e89b49087528
ImDong <www@qs5.org>
2021-07-28 23:35:24 +0800

    添加 读取配置的罗就

--- build/preload.js
+++ build/preload.js
@@ -2,7 +2,14 @@ const crypto = require('crypto');
 const https = require('https');
 const fs = require('fs');
 
+// 检查配置文件是否存在
+let config = {};
+if (fs.existsSync(__dirname + '/config.js')) {
+    config = require('./config');
+}
+
 window.exports = {
     crypto: crypto,
     https: https,
+    config: config,
 }
