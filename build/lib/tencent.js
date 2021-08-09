51db0b1bae70ea1f607742d0b2e78b65ec38b481
ImDong <www@qs5.org>
2021-08-09 21:29:59 +0800

    调整腾讯翻译支持新模式

--- build/lib/tencent.js
+++ build/lib/tencent.js
@@ -29,7 +29,7 @@
         const httpRequestMethod = "POST"
         const canonicalUri = "/"
         const canonicalQueryString = ""
-        const canonicalHeaders = "content-type:application/json; charset=utf-8\n" + "host:" + endpoint + "\n"
+        const canonicalHeaders = "content-type:application/json; charset=utf-8\n" + "host:" + sdk.options.endpoint.value + "\n"
 
         const canonicalRequest = httpRequestMethod + "\n"
             + canonicalUri + "\n"
@@ -42,7 +42,7 @@
         // ************* 步骤 2：拼接待签名字符串 *************
         const algorithm = "TC3-HMAC-SHA256"
         const hashedCanonicalRequest = getHash(canonicalRequest);
-        const credentialScope = date + "/" + service + "/" + "tc3_request"
+        const credentialScope = date + "/" + sdk.options.service.value + "/" + "tc3_request"
         const stringToSign = algorithm + "\n" +
             timestamp + "\n" +
             credentialScope + "\n" +
@@ -50,15 +50,15 @@
         console.log(stringToSign)
 
         // ************* 步骤 3：计算签名 *************
-        const kDate = sha256(date, 'TC3' + SECRET_KEY)
-        const kService = sha256(service, kDate)
+        const kDate = sha256(date, 'TC3' + sdk.options.secret_key.value)
+        const kService = sha256(sdk.options.service.value, kDate)
         const kSigning = sha256('tc3_request', kService)
         const signature = sha256(stringToSign, kSigning, 'hex')
         console.log(signature)
 
         // ************* 步骤 4：拼接 Authorization *************
         const authorization = algorithm + " " +
-            "Credential=" + SECRET_ID + "/" + credentialScope + ", " +
+            "Credential=" + sdk.options.secret_id.value + "/" + credentialScope + ", " +
             "SignedHeaders=" + signedHeaders + ", " +
             "Signature=" + signature
 
@@ -79,35 +79,86 @@
         title: "腾讯翻译",
         languages: ['auto', 'zhcn', 'en'],
         options: {
-            'secret_id': {}
-        }, 
+            secret_id: {
+                type: 'text',
+                label: 'Secret ID',
+                value: null
+            },
+            secret_key: {
+                type: 'text',
+                label: 'Secret Key',
+                value: null
+            },
+            endpoint: {
+                // 只读
+                type: 'only_read',
+                label: 'Endpoint',
+                value: 'tmt.tencentcloudapi.com'
+            },
+            region: {
+                type: 'only_read',
+                label: 'Region',
+                value: 'ap-guangzhou',
+            },
+            service: {
+                type: 'hidden',
+                label: 'Service',
+                value: 'tmt'
+            },
+            action: {
+                type: 'hidden',
+                label: 'Action',
+                value: 'TextTranslate'
+            },
+            version: {
+                type: 'hidden',
+                label: 'Version',
+                value: '2018-03-21'
+            }
+        },
         is_default: true, // 表示自己希望成为默认值
     };
 
-    sdk.prototype.go = function (text, source, to, cb) {
+
+    /**
+     * 执行翻译的对象
+     * @param {string} text 要翻译的文本
+     * @param {string} source 源语言
+     * @param {string} to 目标语言
+     */
+    sdk.go = function (text, source, to) {
+        console.log(this)
+
+
         let payload = JSON.stringify({
             "SourceText": text,
             "Source": languageMap[source],
             "Target": languageMap[to],
             "ProjectId": 0
         }),
-        request = build_request(payload);
+            request = build_request(payload);
 
-        ajax("https://" + endpoint, payload, result => {
-            let data = JSON.parse(result);
-            cb(data.Response.TargetText);
-        }, xhr => {
-            xhr.setRequestHeader('Authorization', request.authorization);
-            xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-            // xhr.setRequestHeader('Host', endpoint);
-            xhr.setRequestHeader('X-TC-Action', action);
-            xhr.setRequestHeader('X-TC-Timestamp', request.timestamp);
-            xhr.setRequestHeader('X-TC-Version', version);
-            xhr.setRequestHeader('X-TC-Region', region);
+        return new Promise((resolve, reject) => {
+            ajax("https://" + sdk.options.endpoint.value, payload, result => {
+                let data = JSON.parse(result).Response;
+
+                if(data.Error) {
+                    reject(data.Error.Message);
+                    return;
+                }
+
+                resolve(data.TargetText);
+            }, xhr => {
+                xhr.setRequestHeader('Authorization', request.authorization);
+                xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                // xhr.setRequestHeader('Host', sdk.options.endpoint.value);
+                xhr.setRequestHeader('X-TC-Action', sdk.options.action.value);
+                xhr.setRequestHeader('X-TC-Timestamp', request.timestamp);
+                xhr.setRequestHeader('X-TC-Version', sdk.options.version.value);
+                xhr.setRequestHeader('X-TC-Region', sdk.options.region.value);
+            });
         });
     }
 
-
     Translate.register(sdk);
 })()
-
