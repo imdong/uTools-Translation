1bd515a6ef587d4e8a572c30e66271954bdec33e
ImDong <www@qs5.org>
2021-08-09 22:12:46 +0800

    调整 百度翻译支持新sdk

--- build/lib/baidu.js
+++ build/lib/baidu.js
@@ -1,25 +1,41 @@
 (function () {
-    const appid = '20210714000888248';
-    const token = 'YJNBayc403EAG6Fesvy9';
+    const token = '';
     const languageMap = {
         zhcn: "zh",
         en: "en",
     };
 
     function buildSignature(post_body) {
-        let body = post_body.appid + post_body.q + post_body.salt + token;
+        let body = post_body.appid + post_body.q + post_body.salt + sdk.options.token.value;
         console.log(body)
         return exports.crypto.createHash("md5").update(body).digest("hex");
     }
 
-    let sdk = function () { }
+    let sdk = {
+        name: "baidu",
+        title: "百度翻译",
+        languages: ['auto', 'zhcn', 'en'],
+        options: {
+            appid: {
+                type: 'text',
+                label: 'appid',
+                value: null
+            },
+            token: {
+                type: 'text',
+                label: 'token',
+                value: null
+            },
+        },
+        is_default: true,
+    }
 
-    sdk.prototype.go = function (text, source, target, cb) {
+    sdk.go = function (text, source, target) {
         let post_body = {
             q: text,
             from: languageMap[source],
             to: languageMap[target],
-            appid: appid,
+            appid: sdk.options.appid.value,
             salt: Math.random().toString().split('.')[1],
         }
 
@@ -30,12 +46,25 @@
             body.push(`${key}=${encodeURIComponent(post_body[key])}`);
         }
 
-        ajax('https://fanyi-api.baidu.com/api/trans/vip/translate', body.join('&'), result => {
-            cb(JSON.parse(result).trans_result[0].dst);
-        }, xhr => {
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        return new Promise((resolve, reject) => {
+            ajax('https://fanyi-api.baidu.com/api/trans/vip/translate', body.join('&'), result => {
+                let data = JSON.parse(result);
+                
+                if(data.error_code){
+                    return reject(data.error_msg);
+                }
+                
+                let dsts = [];
+                data.trans_result.forEach(item => {
+                    dsts.push(item.dst);
+                });
+                resolve(dsts.join("\n"));
+            }, xhr => {
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            })
         })
     }
 
-    regSDK('baidu', new sdk(), "百度翻译", true);
+    // 注册
+    Translate.register(sdk);
 })()
