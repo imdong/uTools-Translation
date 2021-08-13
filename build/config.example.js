63eae60ad7940e34a16dbbb1918c76a5ca8ce740
ImDong <www@qs5.org>
2021-08-13 20:50:02 +0800

    添加 配置文件的示例模板

---
+++ build/config.example.js
@@ -0,0 +1,22 @@
+// 将本文件重命名为 config.js
+const config = {
+    options: {
+        tencent: {
+            secret_id: '',
+            secret_key: ""
+        },
+        aliyun: {
+            access_key_id: '',
+            access_key_secret: ''
+        },
+        caiyun: {
+            token: ''
+        },
+        baidu: {
+            appid: '',
+            token: ''
+        },
+    }
+}
+
+module.exports = config;
